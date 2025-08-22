d3c7da526b7094c37ffdd02977cfda1f39488855
Coro <valeriucolt@gmail.com>
2025-08-22 17:08:19 +0300

    movie  commit

--- src/components/VideoPinSection.jsx
+++ src/components/VideoPinSection.jsx
@@ -34,10 +34,9 @@ const VideoPinSection = () => {
             ? "circle(100% at 50% 50%)"
             : "circle(6% at 50% 50%)",
         }}
-        // className="size-full video-box"
-       className="relative w-full h-full min-h-screen video-box"
+        className="size-full video-box" 
       >
-        <video src="/videos/pin-video.mp4" playsInline muted loop autoPlay   className="w-full h-full object-cover" />
+        <video src="/videos/pin-video.mp4" playsInline muted loop autoPlay  />
 
         <div className="abs-center md:scale-100 scale-200">
           <img src="/images/circle-text.svg" alt="" className="spin-circle" />
